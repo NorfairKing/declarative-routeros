271c13889da22b0b686242509bf7596872a322c1
Tom Sydney Kerckhove <syd@cs-syd.eu>
2023-07-07 13:12:09 +0200

    fixed the nix build

--- declarative-routeros/default.nix
+++ declarative-routeros/default.nix
@@ -1,10 +1,21 @@
 { rustPlatform
+  # System dependencies
+, pkg-config
+, openssl
 }:
 rustPlatform.buildRustPackage {
   pname = "declarative-routeros";
   version = "0.0.0";
 
   src = ./.;
+
+  nativeBuildInputs = [
+    pkg-config
+  ];
+
+  buildInputs = [
+    openssl
+  ];
 
   cargoLock = {
     lockFile = ./Cargo.lock;
